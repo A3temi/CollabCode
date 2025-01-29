cd0338794c1110c1e37945cb097cfbc061505793
A3temii <artemiibakharev@gmail.com>
2025-01-30 00:47:20 +0800

    push new dist

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { spawn } = require("child_process");
 const path = require("path");
